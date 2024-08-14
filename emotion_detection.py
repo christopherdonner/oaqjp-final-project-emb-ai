2d62ff23b55f7af186300969d9972f80f412ea5f
Christopher Donner <cdonner@opentext.com>
2024-08-14 09:55:23 -0400

    added response formatting and dominant emotion logic

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,4 +8,17 @@ def emotion_detector(text_to_analyze):
     response = requests.post(URL, json=inputJSON, headers=header)
     result=json.loads(response.text)
     emotion_list=result['emotionPredictions']
-    return result['emotionPredictions']
+    emotion=emotion_list[0]['emotion']
+    sortedEmotions=sorted(emotion.items(), key=lambda x:x[1], reverse=True)
+    print(sortedEmotions)
+    emotion_aggregated={
+        'anger': emotion['anger'],
+        'disgust': emotion['disgust'],
+        'fear': emotion['fear'],
+        'joy': emotion['joy'],
+        'sadness': emotion['sadness'],
+    }
+    print(emotion_aggregated)
+    return emotion_aggregated
+
+emotion_detector("I love this stuff!")
